a8da1962f7aa5517306259d6b7245851a107bc6a
David Price <davidp809@gmail.com>
2018-06-06 21:26:03 -0400

    Most Up-to-date code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');  
 var app = express();  
 var http = require('http');
-var server = require('http').createServer(app);  
+var server = http.createServer(app);  
 var io = require('socket.io')(http).listen(server);
 
 app.use(express.static(__dirname + '/bower_components'));  
